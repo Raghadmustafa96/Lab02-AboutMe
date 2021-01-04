ec9b1522ee1e7846e45cf26b904ec1303e7c35e6
Raghad <raghadmustafa96@yahoo.com>
2021-01-04 17:04:03 +0200

    add lab 2

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//function
+function takeUserInput(message){
+  return prompt(message);
+}
+
+function getValidationInfoInput(userInput , message){
+  while(userInput === undefined || userInput === '' || userInput === null){
+    var userInput = takeUserInput(message);
+  }
+  return userInput;
+}
